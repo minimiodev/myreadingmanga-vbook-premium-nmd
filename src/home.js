2fa1561c9ac74cc934e1cd3a7cfe7d7325a56216
Nguyễn Mạnh Dũng <nguyenmanhdung@users.noreply.github.com>
2026-05-27 12:38:30 +0700

    Add full list of categories and tags to home screen

--- src/home.js
+++ src/home.js
@@ -7,27 +7,97 @@ function execute() {
         },
         {
             title: "Yaoi",
-            input: "/genre/yaoi/",
+            input: "/category/yaoi/",
             script: "gen.js"
         },
         {
-            title: "Bara",
-            input: "/genre/bara/",
+            title: "Yaoi Manga",
+            input: "/category/yaoi-manga/",
+            script: "gen.js"
+        },
+        {
+            title: "Yaoi Doujinshi",
+            input: "/category/yaoi-doujinshi/",
+            script: "gen.js"
+        },
+        {
+            title: "Bara Manga",
+            input: "/category/bara-manga/",
+            script: "gen.js"
+        },
+        {
+            title: "Bara Doujinshi",
+            input: "/category/bara-doujinshi/",
             script: "gen.js"
         },
         {
             title: "Doujinshi",
-            input: "/genre/doujinshi/",
+            input: "/category/doujinshi/",
             script: "gen.js"
         },
         {
-            title: "Tiếng Anh",
-            input: "/genre/english/",
+            title: "Manga",
+            input: "/category/manga/",
+            script: "gen.js"
+        },
+        {
+            title: "Manhwa",
+            input: "/category/manhwa/",
+            script: "gen.js"
+        },
+        {
+            title: "Manhua",
+            input: "/category/manhua/",
+            script: "gen.js"
+        },
+        {
+            title: "Gay Comics",
+            input: "/category/gay-comics/",
+            script: "gen.js"
+        },
+        {
+            title: "CG Art",
+            input: "/category/cg-art/",
+            script: "gen.js"
+        },
+        {
+            title: "Cosplay",
+            input: "/category/cosplay/",
+            script: "gen.js"
+        },
+        {
+            title: "Non-H",
+            input: "/category/non-h/",
+            script: "gen.js"
+        },
+        {
+            title: "Non-Gay",
+            input: "/category/non-gay/",
+            script: "gen.js"
+        },
+        {
+            title: "Video / Game",
+            input: "/category/video-anime-games/",
+            script: "gen.js"
+        },
+        {
+            title: "Furry",
+            input: "/tag/furry/",
+            script: "gen.js"
+        },
+        {
+            title: "Kemono",
+            input: "/tag/kemono/",
             script: "gen.js"
         },
         {
             title: "Tiếng Việt",
             input: "/tag/vietnamese/",
+            script: "gen.js"
+        },
+        {
+            title: "Tiếng Anh",
+            input: "/tag/english/",
             script: "gen.js"
         }
     ]);
